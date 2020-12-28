311859a1e1617e45cf7e26f1af0f911d1ba6a0fc
tosh223 <rev.to12@gmail.com>
2020-12-29 00:51:36 +0900

    update unit-testing by SAM

--- aws/src/handler.py
+++ aws/src/handler.py
@@ -1,11 +1,21 @@
+import os
 import boto3
+from botocore.config import Config
 from pq_converter import LambdaProcessor
+
+endpoint = os.environ["ENDPOINT"]
+endpoint_url = os.environ["ENDPOINT_URL"]
 
 # boto3 session
 session = boto3.Session()
-s3 = session.resource("s3")
+
+if endpoint == "localstack":
+    print("Start Testing with Localstack")
+    s3 = session.resource("s3", endpoint_url=endpoint_url, config=Config())
+else:
+    s3 = session.resource("s3")
 
 
 def lambda_handler(event, context):
-    processor = LambdaProcessor(event, context, s3)
+    processor = LambdaProcessor(event=event, context=context, s3=s3)
     processor.main()
