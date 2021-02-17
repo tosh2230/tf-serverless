23eca0413d32cc79d21df68f634c2ec35880368a
tosh223 <rev.to12@gmail.com>
2021-02-18 00:00:40 +0900

    add cloud run

--- gcp/main.tf
+++ gcp/main.tf
@@ -12,7 +12,7 @@ terraform {
 
 resource "google_storage_bucket" "csv_bucket" {
   name          = "${var.project}-csv-bucket"
-  location      = "us-east1"
+  location      = var.region
   storage_class = "REGIONAL"
 }
 
@@ -24,7 +24,7 @@ data "archive_file" "py_function_archive" {
 
 resource "google_storage_bucket" "zip_bucket" {
   name          = "${var.project}-zip-bucket"
-  location      = "us-east1"
+  location      = var.region
   storage_class = "REGIONAL"
 }
 
@@ -71,4 +71,40 @@ resource "google_cloudfunctions_function_iam_member" "member" {
   cloud_function = google_cloudfunctions_function.pq-converter.name
   role           = "roles/cloudfunctions.invoker"
   member         = "serviceAccount:${google_service_account.sa.email}"
+}
+
+resource "google_cloud_run_service" "helloworld" {
+  name     = "helloworld"
+  location = var.region
+  template {
+    spec {
+      containers {
+        image = "gcr.io/${var.project}/helloworld"
+      }
+    }
+  }
+  traffic {
+    percent         = 100
+    latest_revision = true
+  }
+}
+
+data "google_iam_policy" "noauth" {
+  binding {
+    role = "roles/run.invoker"
+    members = [
+      "allUsers",
+    ]
+  }
+}
+# Enable public access on Cloud Run service
+resource "google_cloud_run_service_iam_policy" "noauth" {
+  location    = google_cloud_run_service.helloworld.location
+  project     = google_cloud_run_service.helloworld.project
+  service     = google_cloud_run_service.helloworld.name
+  policy_data = data.google_iam_policy.noauth.policy_data
+}
+
+output "url" {
+  value = google_cloud_run_service.helloworld.status[0].url
 }
